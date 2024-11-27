5ee84e83d90e21d266d2b8c09bc5e73a3f15b452
Justin <Zhao3434@gmail.com>
2024-11-27 15:41:44 -0500

    initialize window view model

---
+++ src/main/java/interface_adapter/window/WindowViewModel.java
@@ -0,0 +1,15 @@
+package interface_adapter.window;
+//CreateTime: 2024-11-27 3:37 p.m.
+
+import interface_adapter.ViewModel;
+
+import java.awt.*;
+
+public class WindowViewModel extends ViewModel<WindowState> {
+
+    public WindowViewModel(String viewName) {
+        super(viewName);
+    }
+
+
+}
